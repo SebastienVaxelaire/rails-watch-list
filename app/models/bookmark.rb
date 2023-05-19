b3d9cc460084fd31f58d55e17b8691de40b6e122
sebastien vaxelaire <svaxelaire@fluence.eu>
2023-05-19 18:52:38 +0200

    Add associations and validations for movies, lists, bookmarks

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :movie_id, uniqueness: { scope: :list_id }
+  validates :comment, length: { minimum: 6,
+    too_long: "%{count} characters is the minimum allowed" }
 end
