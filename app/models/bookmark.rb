a2db984688bdfdda92b890cb49b1e6ad905e035d
sebastien vaxelaire <svaxelaire@fluence.eu>
2023-05-20 10:03:07 +0200

    Routes for lists and bookmarks

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,6 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :movie_id, uniqueness: { scope: :list_id }
-  validates :comment, length: { minimum: 6,
-    too_long: "%{count} characters is the minimum allowed" }
+  validates :comment, length: { minimum: 6, too_long: '%{count} characters is the minimum allowed' }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'uri'
+require 'net/http'
+require 'openssl'
+
+puts "Delete database"
+Movie.destroy_all
+
+puts "Creating database"
+
+url = URI("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+
+request = Net::HTTP::Get.new(url)
+request["accept"] = 'application/json'
+request["Authorization"] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOTFiNzRiMjkyNDYxYzI3MTIxZDRhYTIyMGQ1YWZjNiIsInN1YiI6IjY0NjdhOWYxZDE4NTcyMDBlNWEzODI5MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Gf82-tAlR52QhVpVqCPhUUjkQ8Xm5QroHEuCDVaIb1c'
+
+response = http.request(request)
+data = JSON.parse(response.read_body)
+# puts response.read_body
+data['results'].each do |result|
+  Movie.create(
+    title: result['title'],
+    overview: result['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{result['poster_path']}",
+    rating: result['vote_average']
+  )
+end
