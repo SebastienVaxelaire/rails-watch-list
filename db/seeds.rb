96cd4d5f32eae6fe9c0e4e87379c2b22e94586ed
sebastien vaxelaire <svaxelaire@fluence.eu>
2023-05-21 17:50:34 +0200

    fix a bug with cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,58 @@ require 'openssl'
 # puts "Delete database"
 # Movie.destroy_all
 
-puts "Creating database"
+puts "Creating database page 1"
+
+url = URI("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+
+request = Net::HTTP::Get.new(url)
+request["accept"] = 'application/json'
+request["Authorization"] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOTFiNzRiMjkyNDYxYzI3MTIxZDRhYTIyMGQ1YWZjNiIsInN1YiI6IjY0NjdhOWYxZDE4NTcyMDBlNWEzODI5MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Gf82-tAlR52QhVpVqCPhUUjkQ8Xm5QroHEuCDVaIb1c'
+
+response = http.request(request)
+data = JSON.parse(response.read_body)
+# puts response.read_body
+data['results'].each do |result|
+  Movie.create(
+    title: result['title'],
+    overview: result['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{result['poster_path']}",
+    backdrop_url: "https://image.tmdb.org/t/p/w500/#{result['backdrop_path']}",
+    rating: result['vote_average']
+  )
+end
+
+
+puts "Creating database page 2"
 
 url = URI("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=2")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+
+request = Net::HTTP::Get.new(url)
+request["accept"] = 'application/json'
+request["Authorization"] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOTFiNzRiMjkyNDYxYzI3MTIxZDRhYTIyMGQ1YWZjNiIsInN1YiI6IjY0NjdhOWYxZDE4NTcyMDBlNWEzODI5MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Gf82-tAlR52QhVpVqCPhUUjkQ8Xm5QroHEuCDVaIb1c'
+
+response = http.request(request)
+data = JSON.parse(response.read_body)
+# puts response.read_body
+data['results'].each do |result|
+  Movie.create(
+    title: result['title'],
+    overview: result['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{result['poster_path']}",
+    backdrop_url: "https://image.tmdb.org/t/p/w500/#{result['backdrop_path']}",
+    rating: result['vote_average']
+  )
+end
+
+puts "Creating database page 3"
+
+url = URI("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=3")
 
 http = Net::HTTP.new(url.host, url.port)
 http.use_ssl = true
